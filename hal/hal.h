5e07758dd713832433b07376ade3564918142a7a
Daniel Eichhorn <dani.eichhorn@squix.ch>
2024-11-16 10:17:29 +0100

    Add documentation and clean up code

--- hal/hal.h
+++ hal/hal.h
@@ -3,5 +3,5 @@
 #if defined(NATIVE)
   #include "mac_arm64/include_hal_mac_arm64.h"
 #elif defined(ARDUINO)
-  #include "ckg-pro/include_hal_ckg_pro.h"
+  #include "esp32/include_hal_esp32.h"
 #endif
